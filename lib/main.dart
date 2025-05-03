ff8b651e34fd38ac314f53f919ed907f94ee713c
BTranBui <buitran0205@gmail.com>
2025-05-04 01:55:43 +0700

    structures

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:t_store/app.dart';
+import 'package:t_store/utils/constants/colors.dart';
+import 'package:t_store/utils/theme/theme.dart';
+
+import 'app.dart';
 
 void main() {
   runApp(const App());
