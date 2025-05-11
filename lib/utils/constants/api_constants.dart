d093389dd1db957d9e4af4c857f2fa6820f58b94
TQBao392 <btranquoc94@gmail.com>
2025-05-11 17:57:20 +0700

    Initial commit

---
+++ lib/utils/constants/api_constants.dart
@@ -0,0 +1,5 @@
+/* --List of constant used in APIs -- */
+
+class APIConstants {
+  static const String tSecretAPIKey = "";
+}
---
+++ lib/utils/constants/text_strings.dart
@@ -0,0 +1,19 @@
+class TTexts {
+  // --GLOBAL Texts
+
+  // --OnBoarding Texts
+  static const String onBoardingTittle1 = "Choose your product";
+  static const String onBoardingTittle2 = "Select Payment Method";
+  static const String onBoardingTittle3 = "Deliver at your door step";
+
+  static const String onBoardingSubTittle1 =
+      "Welcome to a World of Litmiless Choices - Your Perfect Product Awaits";
+  static const String onBoardingSubTittle2 =
+      "For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority";
+  static const String onBoardingSubTittle3 =
+      "From Our DoorStep to Yours - Swift, Sercure, and Contactless Delivery";
+
+  // --Home
+  static const String homeAppbarTittle = "Good day for shopping";
+  static const String homeAppbarSubTittle = "Taimor Sikander";
+}
