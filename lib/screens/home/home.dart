0b8161a1722276cda3391a39b1292f4f398b4f92
tk <milkcucheo@gmail.com>
2025-05-18 16:45:08 +0700

    home

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:sneakerhead/common/widgets/appbar/appbar.dart';
-import 'package:sneakerhead/common/widgets/custom_shapes/containers/circular_container.dart';
-import 'package:sneakerhead/common/widgets/custom_shapes/curved_edges/curved_edge_widget.dart';
-import 'package:sneakerhead/common/widgets/custom_shapes/curved_edges/curved_edges.dart';
 import 'package:sneakerhead/common/widgets/custom_shapes/containers/primary_header_container.dart';
 import 'package:sneakerhead/utils/constants/colors.dart';
-
+import 'package:sneakerhead/utils/constants/text_strings.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -18,23 +15,71 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             TPrimaryHeaderContainer(
-                child: Column(
-                  children: [
-                    TAppBar(title: Column(
+              child: Column(
+                children: [
+                  TAppBar(
+                    title: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text(TTexts.homeAppbarTitle, style: Theme.of(context).textTheme.headlineSmall),
-                        const SizedBox(height: 5),
-                        Text('Your one-stop shop for sneakers', style: Theme.of(context).textTheme.bodyMedium)
+                        Text(
+                          TTexts.homeAppbarTittle,
+                          style: Theme.of(context).textTheme.labelMedium?.apply(color: TColors.grey),
+                        ),
+                        Text(
+                          TTexts.homeAppbarSubTittle,
+                          style: Theme.of(context).textTheme.headlineSmall?.apply(color: TColors.white),
+                        ),
                       ],
-                    )),
-                    ),)
-                  ],
-                )
+                    ),
+                    actions: [
+                      TCartCounterIcon(onPressed: (){}, iconColor: TColors.white),
+                    ],
+                  ),
+                ],
               ),
-            ],
+            ),
+          ],
         ),
-    ),
+      ),
     );
   }
 }
 
+class TCartCounterIcon extends StatelessWidget {
+  const TCartCounterIcon({
+    super.key,
+    required this.onPressed, required this.IconColor,
+  });
+
+  final Color IconColor;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        IconButton(
+          onPressed: onPressed,
+          icon: Icon(Iconsax.shopping_bag, color: iconColor)
+        ),
+        Positioned(
+          right: 0,
+          child: Container(
+            width: 18,
+            height: 18,
+            decoration: BoxDecoration(
+              color: TColors.black,
+              borderRadius: BorderRadius.circular(100),
+            ),
+            child: Center(
+              child: Text(
+                '2',
+                style: Theme.of(context).textTheme.labelLarge?.apply(color: TColors.white),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
