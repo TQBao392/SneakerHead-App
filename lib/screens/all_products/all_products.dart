193b9ba2a05008a1df432a58f1218aa4c993b596
tk <milkcucheo@gmail.com>
2025-05-22 05:41:47 +0700

    all product screen

--- lib/screens/all_products/all_products.dart
+++ lib/screens/all_products/all_products.dart
@@ -1,17 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:sneakerhead/common/widgets/appbar/appbar.dart';
+import 'package:sneakerhead/utils/constants/sizes.dart';
+import 'package:sneakerhead/screens/sub_category/sub_categories.dart';
 
 class AllProducts extends StatelessWidget {
   const AllProducts({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: TAppBar(title: Text('Popular Products'), showBackArrow: true),
-      );
-      body: const Center(
-        child: Text('All Products'),
-      ),
+    return const Scaffold(
+      appBar: TAppBar(
+        title: Text('All Products'),
+        showBackArrow: true),
+        body: SingleChildScrollView(
+          child: Padding(padding: const EdgeInsets.all(TSizes.defaultSpace),
+            child: Column(
+              children: [
+                /// Dropdown
+                DropDownButtonFormField(
+                  decoration: InputDecoration(prefixIcon: Icon(Iconsax.sort)),
+                  onChanged: (value) {},
+                  items: ['Name', 'Higher Price', 'Lower Price', 'Sale', 'Newest', 'Popularity']
+                      .map((option) => DropdownMenuItem(value: option, child: Text(option)))
+                      .toList(),
+                ),
+                const SizedBox(height: TSizes.spaceBtwSections),
+                /// Products
+                TGridLayout()
+              ],
+            ),
+            ),
+        ),
     );
   }
 }
