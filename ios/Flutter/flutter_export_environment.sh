19f67afdd34466008297c8dad2371dc91644d0cf
VuQuangMinh1212 <vuquangminh12122004@gmail.com>
2025-05-27 23:14:36 +0700

    Update

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\flutter_windows_3.29.2-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\sneakerhead"
+export "FLUTTER_ROOT=D:\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\a"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
