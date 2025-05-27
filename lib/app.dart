9edcfdefc0325692309eb4b0c20adabf1c92a767
tk <milkcucheo@gmail.com>
2025-05-27 22:44:58 +0700

    fix signup controller

--- lib/app.dart
+++ lib/app.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:t_store/utils/constants/text_strings.dart';
-import 'package:t_store/utils/theme/theme.dart';
+import '../utils/theme/theme.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:t_store/utils/constants/colors.dart';
-import 'package:t_store/utils/theme/theme.dart';
+import '../utils/constants/colors.dart';
+import '../utils/theme/theme.dart';
+
 
 import 'app.dart';
 
